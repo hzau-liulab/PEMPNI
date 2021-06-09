454124729bc9c9a8c208a183b39108713f20c45c
hzau-liulab <81418793+hzau-liulab@users.noreply.github.com>
2021-06-09 16:57:46 +0800

    Add files via upload

---
+++ scripts/MPD-regression.py
@@ -0,0 +1,38 @@
+import numpy as np
+from scipy import stats
+from sklearn.ensemble import RandomForestRegressor
+import sys
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import mean_squared_error
+import math
+import re
+import joblib
+
+if __name__ == '__main__':
+    workdir = sys.argv[1]
+    
+    EPI_regression = joblib.load(workdir+'/models/MPD-EPI-Regression.m')
+    ETOR_regression = joblib.load(workdir+'/models/MPD-ETOR-Regression.m')
+    Nonenergy_regression = joblib.load(workdir+'/models/MPD-Nonenergy-Regression.m')
+    
+    nonenergy_fea = np.loadtxt(workdir+'/output/bio_feature/bio_feature.txt',dtype=bytes).astype(str)
+    EPI_fea = np.loadtxt(workdir+"/output/energy_feature/GB1/EPI.txt",dtype=bytes).astype(str)
+    ETOR_fea = np.loadtxt(workdir+"/output/energy_feature/GB1/ETOR.txt",dtype=bytes).astype(str)
+
+    test_nonenergy = []
+    test_nonenergy.append(nonenergy_fea[1:])
+    prediction_nonenergy = Nonenergy_regression.predict(test_nonenergy)
+
+    test_EPI = []
+    test_EPI.append(EPI_fea[:])
+    prediction_EPI = EPI_regression.predict(test_EPI)
+
+    test_ETOR = []
+    test_ETOR.append(ETOR_fea[:])
+    prediction_ETOR = ETOR_regression.predict(test_ETOR)
+
+    predition_energy = 0.4*prediction_ETOR + 0.6*prediction_EPI
+    prediction = 0.6*predition_energy + 0.4*prediction_nonenergy
+
+    with open(workdir+"/output/result.txt","a") as fil:
+        fil.write("\n"+str("Predicted affinity change:")+str('%.3f' % prediction[0]))
---
+++ scripts/MPR-regression.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import stats
+from sklearn.ensemble import RandomForestRegressor
+import sys
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import mean_squared_error
+import math
+import re
+import joblib
+
+if __name__ == '__main__':
+    workdir = sys.argv[1]
+    
+    EWC_regression = joblib.load(workdir+'/models/MPR-EWC-Regression.m')
+    Nonenergy_regression = joblib.load(workdir+'/models/MPR-Nonenergy-Regression.m')
+    
+    nonenergy_fea = np.loadtxt(workdir+'/output/bio_feature/bio_feature.txt',dtype=bytes).astype(str)
+    EWC_fea = np.loadtxt(workdir+"/output/energy_feature/GB2/EWC.txt",dtype=bytes).astype(str)
+
+    test_nonenergy = []
+    test_nonenergy.append(nonenergy_fea[1:])
+    prediction_nonenergy = Nonenergy_regression.predict(test_nonenergy)
+
+    test_EWC = []
+    test_EWC.append(EWC_fea[:])
+    prediction_energy = EWC_regression.predict(test_EWC)
+
+    prediction = 0.5*prediction_energy + 0.5*prediction_nonenergy
+
+    with open(workdir+"/output/result.txt","a") as fil:
+        fil.write("\n"+str("Predicted affinity change:")+str('%.3f' % prediction[0]))
