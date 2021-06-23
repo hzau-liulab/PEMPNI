0e315a805373e16ae4951c1460f2229a3927cf54
hzau-liulab <81418793+hzau-liulab@users.noreply.github.com>
2021-06-23 11:13:07 +0800

    Delete examples/protein-DNA/energy_feature/GB1 directory

--- examples/protein-DNA/energy_feature/GB1/wt/amber.sh
+++
@@ -1,8 +0,0 @@
-#! /user/bin/sh
-path="/home/yaojiang/amber16";
-$path/bin/tleap -f tleap.in >>log.txt;
-$path/bin/sander -O -i min1.in -o min1.out -p com_solvated.prmtop -c com_solvated.inpcrd -r min1.rst -ref com_solvated.inpcrd;
-$path/bin/sander -O -i min2.in -o min2.out -p com_solvated.prmtop -c min1.rst -r min2.rst -ref min1.rst;
-$path/bin/sander -O -i min3.in -o min3.out -p com_solvated.prmtop -c min2.rst -r min3.rst -ref min2.rst;
-$path/bin/cpptraj -i cpptraj.in >>log.txt;
-$path/bin/MMPBSA.py -O -i mmpbsa_residue_pair.in -o Final_result_mmpbsa_GB1_pair.dat -do FINAL_DECOMP_MMPBSA_GB1_pair.dat -sp com_solvated.prmtop -cp com.prmtop -rp pro.prmtop -lp dna.prmtop -y last.crd > mmpbsa.log;
